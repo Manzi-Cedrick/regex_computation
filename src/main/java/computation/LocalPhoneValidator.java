2d3f18c409157a24169de93385b445a12833e28a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-05-23 10:49:18 +0200

    [feat]: local phone validation init

---
+++ src/main/java/computation/LocalPhoneValidator.java
@@ -0,0 +1,6 @@
+package computation;
+
+public class LocalPhoneValidator {
+    private static final String LOCAL_PHONE_REGEX = "^07\\d{8}$";
+    private static final String INTERNATIONAL_REGEX = "^2507\\d{8}$";
+}
